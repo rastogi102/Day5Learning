8f8889a47d7ae9ab483c8f2723c4722a92482539
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-14 10:18:03 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noc, nos;
+            Console.WriteLine("Enter number of classes");
+            noc = int.Parse(Console.ReadLine());
+            string[][] students = new string[noc][];
+            for (int i = 0; i < noc; i++)
+            {
+                Console.WriteLine("Enter number of students");
+                nos = int.Parse(Console.ReadLine());
+                students[i] = new string[nos];
+                for (int j = 0; j < nos; j++)
+                {
+                    Console.WriteLine($"Enter studen{j + 1}\'s name");
+                    students[i][j] = Console.ReadLine();
+
+                }
+            }
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"Students list of class{i + 1}");
+                Console.WriteLine("*************************************");
+                for (int j = 0; j < students[i].Length; j++)
+                {
+                    Console.WriteLine(students[i][j]);
+                }
+                Console.WriteLine("***************************************");
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+    }
+}
